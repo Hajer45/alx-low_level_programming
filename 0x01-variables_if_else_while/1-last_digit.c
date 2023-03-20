0ef39bb1bfad13ec801694ed340ca4e1c069f236
Hajer Talbi <hagartalbi@gmail.com>
2023-03-20 13:50:36 -0700

    Task 1 modified

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,6 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-
 /* betty style doc for function main goes there */
 /**
  * main - Entry point
@@ -21,7 +20,7 @@ if (n % 10 == 0)
 printf(" and is 0\n");
 else if ((n % 10) > 5)
 printf(" and is greater than 5\n");
-else 
+else
 printf(" and is less than 6 and not 0\n");
 return (0);
 }
