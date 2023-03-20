ed8b99b1722deb1dfd7b74ede6ec4f0cd164678a
Hajer Talbi <hagartalbi@gmail.com>
2023-03-20 16:58:41 -0700

    Task 8 done :)

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a = 0;
+char c = 'a';
+while (a <= 9)
+{
+putchar('0'+a);
+a++;
+}
+while (c <= 'f')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
