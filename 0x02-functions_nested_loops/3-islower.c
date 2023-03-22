1f86dbc5c081892d11d4fa5d56aa13f089679ba4
Hajer Talbi <hagartalbi@gmail.com>
2023-03-22 14:28:03 -0700

    task 3 done

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 /**
-*_islower - test if c is lower 
-*@c: int number
-*Return: 0 if not lower 1 else
+ *_islower - test if c is lower 
+ *@c: int number
+ *Return: 0 if not lower 1 else
 */
 int _islower(int c)
 {
