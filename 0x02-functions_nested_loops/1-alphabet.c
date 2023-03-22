0d6aebd4fa4445ecbc61079b5cd7beac13182265
Hajer Talbi <hagartalbi@gmail.com>
2023-03-22 10:36:32 -0700

    task 1 done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "main.h"
-/*function to print lower alphabet in one ligne*/
+
+/**
+ * print alphabet
+ *
+ * Return: Always void.
+ */
 void print_alphabet(void)
 {
 char c = 'a';
