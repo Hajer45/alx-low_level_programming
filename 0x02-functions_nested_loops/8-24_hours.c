c25e9b12909d5115dd85299ae3b075e1e5988d21
Hajer Talbi <hagartalbi@gmail.com>
2023-03-22 17:06:55 -0700

    task 8 done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,9 +7,9 @@
 void jack_bauer(void)
 {
 int hour, minute;
-for (hour = 0; hour < 24; hour++) 
+for (hour = 0; hour < 24; hour++)
 {
-for (minute = 0; minute < 60; minute++) 
+for (minute = 0; minute < 60; minute++)
 {
 _putchar('0' + (hour / 10));
 _putchar('0' + (hour % 10));
