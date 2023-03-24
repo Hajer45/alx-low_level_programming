fc1be2d1fe11d424a8b6145362426d082a871ee8
Hajer Talbi <hagartalbi@gmail.com>
2023-03-24 04:45:08 -0700

    task 7 done

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,10 +13,10 @@ void print_diagonal(int n)
 	for (i = 0; i < n; i++)
 	{
 		_putchar('\\');
-		if (n > 1 && j!= (n - 1))
+		if (n > 1 && j != (n - 1))
 		{
 		_putchar('\n');
-		for (j =0; j <= i; j++)
+		for (j = 0; j <= i; j++)
 		_putchar(' ');
 		}
 	}
