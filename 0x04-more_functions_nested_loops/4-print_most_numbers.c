6b341450e6ff25335723cee73535554f20e2f7bf
Hajer Talbi <hagartalbi@gmail.com>
2023-03-24 03:41:52 -0700

    task 4 done

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * prints numbers
+ * Return: Always 0 (Success)
+ **/
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+	if (x != 2 && x != 4)
+	{
+	_putchar('0' + x);
+	}
+	}
+	_putchar('\n');
+}
