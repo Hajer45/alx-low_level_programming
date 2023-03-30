b6dda34c9472bea23c36dfb06c40b6f64ca2310f
Hajer Talbi <hagartalbi@gmail.com>
2023-03-30 16:43:36 -0700

    task 3 done

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+/**
+ * _strcmp - compare tow strings
+ *@s1: pointer to char
+ *@s2: pointer to char
+ * Return: Always int (Success)
+ **/
+
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+
+while (s1[i] != '\0' || s2[i] != '\0')
+{
+if (s1[i] < s2[i])
+{
+return (-1);
+}
+else if (s1[i] > s2[i])
+{
+return (1);
+}
+i++;
+}
+return (0);
+}
