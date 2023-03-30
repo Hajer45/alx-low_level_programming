84bff64dca4e93cf6b9f4525bfdb5fb698af91aa
Hajer Talbi <hagartalbi@gmail.com>
2023-03-30 16:32:30 -0700

    task 3 done

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,25 @@
 #include "main.h"
 
 /**
- * _strncat - Entry point
+ * _strncpy - Entry point
  *@dest: pointer to char
  *@src: pointer to char
  *@n: int
  * Return: Always pointer to char (Success)
  **/
-char* _strncpy(char* dest, char* src, int n) {
-    int i;
+char *_strncpy(char *dest, char *src, int n)
+{
 
-    for (i = 0; i < n && src[i] != '\0'; i++) 
-    {
-        dest[i] = src[i];
-    }
-    for ( ; i < n; i++) 
-    {
-        dest[i] = '\0';
-    }
-    return dest;
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+for ( ; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
 }
 
