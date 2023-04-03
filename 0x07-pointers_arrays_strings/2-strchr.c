245c198bcff66de62daa3d22f0174f5fc28c8b40
Hajer Talbi <hagartalbi@gmail.com>
2023-04-03 13:31:28 -0700

    task 2done

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - pointer to the first occurrence of the character c in the string s
+ *@s : pointer to char
+ *@c : char
+ * Return: always pointer to char
+ **/
+
+char *_strchr(char *s, char c)
+{
+char *p = NULL;
+while (*s != '\0')
+{
+if (*s == c)
+return (s);
+}
+return (p);
+}
