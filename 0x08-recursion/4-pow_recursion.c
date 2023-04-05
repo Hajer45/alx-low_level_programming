17e70a15a8b5f3a3f6bd79daa4385f676424632f
Hajer Talbi <hagartalbi@gmail.com>
2023-04-05 16:52:54 -0700

    task 4 done

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  _pow_recursion -   calculate x power y
+ *@x : int
+ *@y : int
+ * Return: always int
+ **/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0
+{
+return (1);
+}
+return (x * _pow_recursion(x, y - 1));
+}
