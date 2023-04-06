73f04a90eb43530ea7c49e564a1424824be6a3b6
Hajer Talbi <hagartalbi@gmail.com>
2023-04-06 14:31:45 -0700

    task  done

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,25 +1,44 @@
-/**
- * _sqrt_recursion - calculate sqrt of n
- *@n : int
- * Return: always int
- **/
+#include "main.h"
 
-int _sqrt_recursion(int n)
+/**
+ * _sqrt_ - Gives square root of number
+ *
+ * @n: int
+ * @k: int
+ *
+ * Return: int
+ */
+
+int _sqrt_(int n, int k)
 {
 if (n < 0)
 {
 return (-1);
 }
-if (n == 0 || n == 1){
-return  (n);
+if ((n) - (k * k) == 0)
+{
+return (k);
 }
-int x = _sqrt_recursion(n / 4) * 2;
-if ((x+1) * (x+1) <= n) {
-return (x + 1);
+if (n < (k * k))
+{
+return (-1);
 }
 else
 {
-return (x);
+return (_sqrt_(n, k + 1));
 }
+}
+
+/**
+ * _sqrt_recursion - Return sqrt of number
+ *
+ * @n: int
+ *
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_(n, 0));
 }
 
